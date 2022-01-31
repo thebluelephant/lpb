6d5382c6415691509f4903319d8e62854a2a5978
Léa <ldelacotte@sqli.com>
2022-01-31 18:49:59 +0100

    Add recommendation carroussel to the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,42 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { fetchHomepageData } from '../helper/Api'
-import '../style/Homepage.scss';
+import React, { useEffect, useState } from "react";
+import RecommendationsCarroussel from "../components/RecommendationCarroussel";
+import { fetchHomepageData } from "../helper/Api";
+import "../style/Homepage.scss";
 
 const Homepage = () => {
-    const [homepageData, setHomepageData] = useState();
-    const [dataLoaded, setDataLoaded] = useState(false);
+  const [homepageData, setHomepageData] = useState();
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-    useEffect(() => {
-        if (!dataLoaded) {
-            fetchHomepageData().then((res) => {
-                setHomepageData(res)
-                console.log(res);
-                setDataLoaded(true)
-            })
-        }
-    }, [dataLoaded]);
+  useEffect(() => {
+    if (!dataLoaded) {
+      fetchHomepageData().then((res) => {
+        setHomepageData(res);
+        setDataLoaded(true);
+      });
+    }
+  }, [dataLoaded]);
 
-    return (
-        <div className="homepage">
-            {
-                homepageData &&
-                <div className="homepage homepage__informations">
-                    <p>{homepageData.welcome_firstline}</p>
-                    <p>{homepageData.welcome_secondline}</p>
-                    <p>{homepageData.welcome_thirdline}</p>
-                    <p>{homepageData.welcome_fourthline}</p>
-                </div>
-            }
-
-            <p className='homepage__phone'>Téléphone : {homepageData?.phone} </p>
-            <img src={'assets/LPB-banniereAccueil-title.png'} className="homepage homepage__image homepage__image--title" alt="le ptit bistrot title" />
-            <h3 className="homepage homepage__title homepage__title--responsive">Le p'tit bistrot</h3>
-            <img src={'assets/LPB-banniereAccueil-subtitle.png'} className="homepage homepage__image homepage__image--subtitle" alt="le ptit bistrot subtitle" />
-            <img src={'assets/LPB-banniereAccueil-cow.png'} className="homepage homepage__image homepage__image--cow" alt="cow" />
-            <img src={'assets/LPB-banniereAccueil-bkg.jpg'} className="homepage homepage__image homepage__image--bkg" alt="background" />
-
+  return (
+    <div className="homepage">
+      {homepageData && (
+        <div className="homepage homepage__informations">
+          <p>{homepageData.welcome_firstline}</p>
+          <p>{homepageData.welcome_secondline}</p>
+          <p>{homepageData.welcome_thirdline}</p>
+          <p>{homepageData.welcome_fourthline}</p>
         </div>
-    );
-}
+      )}
+      <div className="homepage__carroussel">
+        <RecommendationsCarroussel />
+      </div>
+      <p className="homepage__phone">Téléphone : {homepageData?.phone} </p>
+      <img
+        src={"assets/LPB-banniereAccueil-title.png"}
+        className="homepage homepage__image homepage__image--title"
+        alt="le ptit bistrot title"
+      />
+      <h3 className="homepage homepage__title homepage__title--responsive">
+        Le p'tit bistrot
+      </h3>
+      <img
+        src={"assets/LPB-banniereAccueil-subtitle.png"}
+        className="homepage homepage__image homepage__image--subtitle"
+        alt="le ptit bistrot subtitle"
+      />
+      <img
+        src={"assets/LPB-banniereAccueil-cow.png"}
+        className="homepage homepage__image homepage__image--cow"
+        alt="cow"
+      />
+      <img
+        src={"assets/LPB-banniereAccueil-bkg.jpg"}
+        className="homepage homepage__image homepage__image--bkg"
+        alt="background"
+      />
+    </div>
+  );
+};
 
 export default Homepage;
